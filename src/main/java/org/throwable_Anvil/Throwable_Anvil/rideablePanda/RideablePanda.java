06ec35d7cdf95119a4904f0cda07cbf31675355e
nosgamingalt <fazeboiz3125@gmail.com>
2025-12-14 19:32:39 -0600

    V1.2

---
+++ src/main/java/org/throwable_Anvil/Throwable_Anvil/rideablePanda/RideablePanda.java
@@ -0,0 +1,17 @@
+package org.throwable_Anvil.Throwable_Anvil.rideablePanda;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RideablePanda extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
